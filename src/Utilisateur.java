dc2f8902e757e5e246713683f6a6eb99ab1fb3c2
Simon Belley <belleysimon44@gmail.com>
2018-10-03 11:20:39 -0400

    did nothing

---
+++ src/Utilisateur.java
@@ -0,0 +1,56 @@
+public class Utilisateur {
+    String prenom;
+    String nom;
+    String nomUtil;
+    String password;
+    String genre;
+    int age;
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNomUtil() {
+        return nomUtil;
+    }
+
+    public void setNomUtil(String nomUtil) {
+        this.nomUtil = nomUtil;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
